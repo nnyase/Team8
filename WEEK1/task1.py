37342d92dae1308efd7a07e098629e6510bec8c9
inigo27 <inigoauz.27@gmail.com>
2022-10-04 19:58:40 +0200

    Descriptors Calculated
    Script of task 1 changed to save the descriptors in a folder

--- WEEK1/task1.py
+++ WEEK1/task1.py
@@ -4,14 +4,31 @@ import os
 
 
 # Read dataset images
-pathDataset = "../BBDD/"
-pathQuery1 = "../qsd1_w1/"
-pathQuery2 = "../qsd2_w1/"
+pathDataset = "../../WEEK1/BBDD/"
+pathQuery1 = "../../WEEK1/qsd1_w1/"
+pathQuery2 = "../../WEEK1/qsd2_w1/"
 
+
+# By giving it a grayscale image, returns the histogram related to that image
+def calculateHistogram(image):
+    # Create empty histogram
+    histogram = np.array([0]*256)
+    
+    # Check every pixel
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            
+            # If the pixel value is not in [0,255] (can be a background pixel) ignore it
+            if image[i,j] <= 255 or image[i,j] >= 0:
+                
+                histogram[image[i,j]] += 1
+            
+
+    return histogram
 
 # By giving it a image, returns the concatenation of probability histograms related 
 # to that image
-def createHistogram(image):
+def createHistogram(image, background = False):
     
     # Save concatenation of histograms
     finalProbabilityHistogram = np.array([], dtype = np.float64)
@@ -19,8 +36,11 @@ def createHistogram(image):
     # For each channel get the histogram
     for i in range(image.shape[2]):
         
-        # Values from 0 to 255
-        histogram, bin_edges = np.histogram(image[:,:,i], bins=range(0,257))
+        # Get the histogram
+        if background:
+            histogram = calculateHistogram(image[:,:,i])
+        else:
+            histogram, _  = np.histogram(image[:,:,i], bins = range(0,257))
         
         # Get probability distribution
         histogram = histogram.astype(np.float32) / (image.shape[0]*image.shape[1])
@@ -30,9 +50,9 @@ def createHistogram(image):
 
     return finalProbabilityHistogram
 
-# By giving it the path dataset, returns a library with every 
-# descriptor of the BBDD
-def computeBBDDdescriptors(pathDataset):
+# By giving it a path to a image, returns a dictionary with every 
+# descriptor of images and saves the descriptors in the output folder
+def computeDescriptors(imagesPath, outputPath):
     
     descriptors = {}
     
@@ -49,14 +69,20 @@ def computeBBDDdescriptors(pathDataset):
             
             
             histogram = createHistogram(image)
+            
+            descriptorPath = outputPath + file[:-4] + ".npy"
+            np.save(descriptorPath, histogram)
+            
             descriptors[file] = histogram
     
     return descriptors
 
 
 
+pathOutputBBDD = "./descriptorsBBDD/"
+pathOutputQ1 = "./descriptorsQ1/"
+pathOutputQ2 = "./descriptorsQ2/"
 
-computeBBDDdescriptors(pathDataset)
-
-
+computeDescriptors(pathDataset, pathOutputBBDD)
+computeDescriptors(pathQuery1, pathOutputQ1)
 
