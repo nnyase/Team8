6f9e0143e72d9bb94f076bfb21650febd934be54
Pierre Lammers <lammers.pierre@gmail.com>
2022-10-05 18:36:42 +0200

    Generating all the png images of binarymask with method 1

--- WEEK1/task5.py
+++ WEEK1/task5.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image as im
+import os
 
 # METHOD 1
 # Obtain the binary mask of an image
@@ -21,16 +23,47 @@ def get_binary_mask(image):
     ret,binary_mask = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
     # Convert 255 into 1 to have a binary mask
-    binary_mask[binary_mask == 255] = 1 
+    #binary_mask[binary_mask == 255] = 1 
 
     #return the binary mask as numpy array
     return binary_mask
 
 
-# EXAMPLE
-# Convert image to the single channel grayscale image
-# image = cv2.imread('WEEK1/test.jpg')
-# get_binary_mask(image)
+# Save the PNG files in q2_result folder
+
+def store_binary_mask():
+    pathDataset = "WEEK1/qsd2_w1/"
+    pathResultset = "WEEK1/q2_result/"
+
+    # Get file names of the database
+    files = os.listdir(pathDataset)
+
+    for file in files:
+            # Check if it is an image 
+            if file[-4:] == ".jpg" or file[-4:] == ".png":
+
+                #store the image
+                image = cv2.imread(pathDataset + file)
+
+                filename = pathResultset + file[:-4] + ".png"
+
+                #save the image in the q2_result folder
+                cv2.imwrite(filename, get_binary_mask(image))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
