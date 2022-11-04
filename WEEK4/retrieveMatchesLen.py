bc4fa8fbc6c545ba2932603d5f667f6deef72d3a
Pierre Lammers <lammers.pierre@gmail.com>
2022-11-04 13:40:10 +0100

    Add new flannMatcher and bestThreshold function to get the best threshold value

--- WEEK4/retrieveMatchesLen.py
+++ WEEK4/retrieveMatchesLen.py
@@ -153,6 +153,61 @@ def brutForceMatcherKnn(path1, path2, des_type):
                 
     return -len(good)
 
+#FLANN based Matcher
+def flannMatcher(path1, path2, des_type):
+
+    """
+    this function takes the descriptor of every feature in first set and is matched with all other features in second set 
+        using some distance calculation, only taking into account those that are below our threshold value 
+
+    ----------
+    path1 : string
+        Path of the folder where .npy descriptor files of the database are stored.
+    path2 : string
+        Path of the folder where .npy descriptor files of the query images are stored.
+    des_type : str
+        Type of the descriptors.
+    Returns
+    -------
+    matches : list of lists (int)
+        Matches found in the image.
+    """
+
+    descriptors1 = np.load(path1)
+    descriptors2 = np.load(path2)
+
+    # FLANN parameters
+    if des_type == 'orb':
+        #For ORB
+        FLANN_INDEX_LSH = 6
+        index_params= dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 6, # 12
+                   key_size = 12,     # 20
+                   multi_probe_level = 1) #2
+    else:
+        #For SIFT, SURF, etc.
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+
+    search_params = dict(checks=50)   # or pass empty dictionary
+    flann = cv.FlannBasedMatcher(index_params,search_params)
+
+    # Empty array => no matching so give a big vector to have a big distance
+    if not (descriptors1.size == 0 or descriptors2.size == 0):
+
+        matches = flann.knnMatch(descriptors1,descriptors2,k=2)
+
+        #Ratio test as per Lowe's paper
+        good = []
+        for i,(m,n) in enumerate(matches):
+            if m.distance < 0.7*n.distance:
+                good.append([getattr(m,'distance')])
+
+        return -len(good)
+
+    else:
+
+        return 0
 
 def saveBestKmatchesLocalDes(bbddDescriptorsPath, qDescriptorsPath, k, matchingFunc, des_type, 
                              discardMinLen):
@@ -186,15 +241,11 @@ def saveBestKmatchesLocalDes(bbddDescriptorsPath, qDescriptorsPath, k, matchingF
     elif matchingFunc == "bfknn":
         matchingFunc = brutForceMatcherKnn
     elif matchingFunc == "flann":
-        matchingFunc = ""
-    elif matchingFunc == "flannknn":
-        matchingFunc = ""
+        matchingFunc = flannMatcher
     
     # Compute number of images in each set
     numBBDD = len(os.listdir(bbddDescriptorsPath))
     numQ = len(os.listdir(qDescriptorsPath))
-    
-    
 
     # Init result list
     result = []
@@ -237,11 +288,30 @@ def saveBestKmatchesLocalDes(bbddDescriptorsPath, qDescriptorsPath, k, matchingF
     
     return result  
 
+def bestThreshold():
 
-# path11="./descriptors/BBDD/local_descriptor/sift/"
-# path22="./descriptors/qsd1_w4/local_descriptor/sift_method4/"
-# pathStore1 = './result/result_sift_brutForceMatcher.pkl' 
-# pathStore2 = './result/gt_corresps.pkl'
+    """
+        This function print the best threshold value and the mapkL that we obtain with this threshold value
+    """
+    maxResult = 0
+    bestThreshold = 0
+
+    actual = read_pkl(pathStore2)
+    for threshold in range(-100,-40,10):
+        result = mapkL(actual, saveBestKmatchesLocalDes(path11, path22, 5, "flann", "sift", threshold), k=5)
+        if result > maxResult:
+            maxResult = result
+            bestThreshold = threshold
+    print("Threshold = " + str(bestThreshold) + " ==> " + str(maxResult))
+
+
+path11="./descriptors/BBDD/local_descriptor/sift_300/"
+path22="./descriptors/qsd1_w4/local_descriptor/sift_method4_300/"
+pathStore1 = './result/result_sift_brutForceMatcher.pkl' 
+pathStore2 = './result/gt_corresps.pkl'
+pathStore3 = './result/result_sift_flann.pkl'
 
 # store_in_pkl(pathStore1, saveBestKmatches(path11, path22, 10, "sift"))
 # print(mapkL(read_pkl(pathStore2), read_pkl(pathStore1), k=5))
+
+#bestThreshold()
